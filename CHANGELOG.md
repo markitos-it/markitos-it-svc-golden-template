12192024eb62e8aef1ccdf5197d8a68307f53601
Marco Antonio - The Way of the Artisan <markitos.es.info@gmail.com>
2026-03-06 15:20:02 +0100

    feat(release): added more appsec. leaks and use GRPC_TLS_ENABLED

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## v0.2.0 (2026-03-06)
+
+### ♻️  Refactor
+
+- (release) using version semver at deploy ([`773f7e8`])
+- (release) release v0.1.0: update .github/workflows/appsec.yaml.off and .github/workflows/publish-image.yaml.off ([`2dfff46`])
+
 ## v0.1.0 (2026-03-06)
 
 ### ♻️  Refactor
--- bin/app/start.sh
+++ bin/app/start.sh
@@ -7,6 +7,7 @@ cd "$(dirname "$0")/../.."
 make proto
 
 export GRPC_PORT=${GRPC_PORT:-3000}
+export GRPC_TLS_ENABLED=${GRPC_TLS_ENABLED:-false}
 export DB_HOST=${DB_HOST:-localhost}
 export DB_PORT=${DB_PORT:-55432}
 export DB_USER=${DB_USER:-markitos-it-svc-goldens}
@@ -14,10 +15,11 @@ export DB_PASS=${DB_PASS:-markitos-it-svc-goldens}
 export DB_NAME=${DB_NAME:-markitos-it-svc-goldens}
 
 echo "🚀 Starting markitos-it-svc-goldens (Go)..."
-echo "📡 GRPC_PORT: $GRPC_PORT"
-echo "📦 DB_HOST..: $DB_HOST:$DB_PORT"
-echo "📦 DB_USER..: $DB_USER"
-echo "📦 DB_NAME..: $DB_NAME"
+echo "📡 GRPC_PORT.......: $GRPC_PORT"
+echo "📦 DB_HOST.........: $DB_HOST:$DB_PORT"
+echo "📦 DB_USER.........: $DB_USER"
+echo "📦 DB_NAME.........: $DB_NAME"
+echo "📡 GRPC_TLS_ENABLED: $GRPC_TLS_ENABLED"
 echo ""
 
 docker compose up
